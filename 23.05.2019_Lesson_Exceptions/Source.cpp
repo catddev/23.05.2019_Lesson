fd0f0d0ae6346c1d862f7f2988f09f5e46e89027
айкеновад <@205_8>
2019-05-23 11:02:25 +0600

    -

--- 23.05.2019_Lesson_Exceptions/Source.cpp
+++ 23.05.2019_Lesson_Exceptions/Source.cpp
@@ -1,28 +1,77 @@
 ﻿#include<iostream>
 #include<exception>
+#include"Vector.h"
 using namespace std;
+
+double division(double a, double b) {
+	if (b == 0) throw runtime_error("divion by zero!!!");
+	return a / b;
+}
+
+//чтобы увидеть все имеющиеся библиотеки:
+//название проекта "Решение ..." правой кнопкой - свойства - исходные файлы отладки - верхнюю строку копируем (адрес) и вставлем в проводник - откроется список библиотек
 
 int main() {
 
-	int a;
-	cin >> a;
+	//int a;
+	//cin >> a;
+
+	//try {
+	//	if (a > 0)
+	//		throw out_of_range("a is negative");//"a is positive";//the way how to handle an exception
+	//	//out_of_range дочерний класс класса exception
+	//	else if (a == 0)
+	//		throw "a is 0";
+	//	else
+	//		throw 404;//or "a is negative"
+
+	//}
+	////catch может быть сколько угодно, для каждого типа ошибки (int, const char*) свой catch!
+	//catch (const char *c) {//если используем текст для возвращения, то всегда const char*, а если указатель то просто char*
+	//	cout << c << endl;
+	//}
+	//catch (out_of_range obj) {
+	//	cout << obj.what() << endl; //what() - общая функция класса exception для всех исключений, показывает что именно вывести, какое сообщение
+	//}
+	//catch (int a) {
+	//	cout << "Integer type error with code " << a << endl;
+	//}
+
+	//double a, b;
+	//try {
+	//	cin >> a >> b;
+	//	double r = division(a, b);
+	//	cout << r * 10 << endl;
+	//}
+	//catch (runtime_error err){ //обработчик для типа ошибки runtime_error, т.е. как именно обработать исключение при улове в функции division
+	//	cout << "Runtime error: " << err.what() << endl;
+	//}
+
+
+
+	my_vector<int> mv; //обязательно подключить СВОЙ класс вектор, с обработкой исключения!
+	for (int i = 0; i < 5; i++)
+		mv.push_back(i);
+	
+	//тест
+	try {
+		for (int i = 0; i < 10; i++)
+			cout << mv[i] << endl;
+	}
+	//улавливаем
+	catch (out_of_range err) {//err - псевдоним
+		cout << err.what();
+	}
 
 	try {
-		if (a > 0)
-			throw "a is positive";//the way how to handle an exception
-		else if (a == 0)
-			throw "a is 0";
-		else
-			throw 404;//or "a is negative"
+		cout << mv[-3] << endl;
+	}
+	catch (out_of_range err) {
+		cout << err.what();
+	}
 
-	}
-	//catch может быть сколько угодно, для каждого типа ошибки (int, const char*) свой catch!
-	catch (const char *c) {//если используем текст для возвращения, то всегда const char*, а если указатель то просто char*
-		cout << c << endl;
-	}
-	catch (int a) {
-		cout << "Integer type error with code " << a << endl;
-	}
+	//после try-catch остальная прога спокойно работает дальше
+	cout << mv[2] << endl;
 
 	system("pause");
 	return 0;
---
+++ 23.05.2019_Lesson_Exceptions/Vector.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+#include <iostream>
+#include<vector>
+#include<exception>
+using namespace std;
+
+out_of_range upper("out of upper bound\n");
+out_of_range lower("out of lower bound\n");
+
+template <typename T>
+class my_vector : public vector<T> {//обязательно наследуем от шаблонного vector<T>
+public:
+	T& operator[](int index);
+};
+
+template<typename T>
+inline T & my_vector<T>::operator[](int index)throw()// явно пропишем что будем обрабатывать исключение - throw()
+{
+	//при обработке исключений, порядок условий СТРОГИЙ - логично должны быть расставлены, сначала меньше нуля, затем больше или равно, иначе будет вылетать на первом же условии
+	if (index < 0)
+		throw lower;
+	else if (index >= this->size())
+		throw upper;//name (upper), type (out_of range) and what() - "out upper bound\n"
+	return this->at(index); //at() готовый метод для возвращения элемента по индексу
+}
